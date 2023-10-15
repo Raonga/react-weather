20346e3b47c575128533c16b67590ec696d5f18a
Raonga <uanraonga@gmail.com>
2023-10-15 22:40:14 +0200

    import WeatherSearch in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./App";
+import reportWebVitals from "./reportWebVitals";
+import WeatherSearch from "./WeatherSearch";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <div>
+        <h1>Weather Search Engine</h1>
+        <WeatherSearch />
+      </div>
+    </div>
   </React.StrictMode>
 );
 
