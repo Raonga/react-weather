74751e90246bd03446d1feb9452afd21d8d11f3b
Raonga <uanraonga@gmail.com>
2023-10-22 22:36:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card mt-5">
+          <div className="card-body">
+            <Weather defaultCity="Windhoek" />
+          </div>
+        </div>
+
+        <footer className="d-flex justify-content-center">
+          <p>
+            <a href="https://github.com/Raonga/react-weather">
+              {" "}
+              open-sourced code{" "}
+            </a>{" "}
+            by Raonga Uanivi
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
