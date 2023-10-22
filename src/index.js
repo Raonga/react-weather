9346a18554b7a23aa2cfebcdb918c9878ede2b25
Raonga <uanraonga@gmail.com>
2023-10-22 20:04:43 +0200

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./App";
+import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
 import WeatherSearch from "./WeatherSearch";
 
