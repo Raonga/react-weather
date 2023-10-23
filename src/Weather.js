e23540363e6bd1690ec866a1dca856260d2801ca
Raonga <uanraonga@gmail.com>
2023-10-23 17:19:44 +0200

    update App.js, Weather.css & Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -54,7 +54,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-dark w-100"
+                className="btn btn-seondary w-100"
               />
             </div>
           </div>
