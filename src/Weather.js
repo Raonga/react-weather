4938e6b53f7f7599df89a2f99f4ca857f6c4dace
Raonga <uanraonga@gmail.com>
2023-10-28 21:12:32 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -14,8 +14,7 @@ export default function Weather(props) {
       temperature: Math.round(response.data.temperature.current),
       humidity: response.data.temperature.humidity,
       description: response.data.condition.description,
-      iconUrl:
-        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png",
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       wind: Math.round(response.data.wind.speed),
       city: response.data.city,
       date: new Date(response.data.time * 1000),
